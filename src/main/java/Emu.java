4e666b68f6945d1fa296d227e507c987c4e7657a
Raylong268 <shanetanjun@gmail.com>
2026-01-16 21:32:23 +0800

    Added implementation to store all text sent to chatbot, and to print all of them in order when 'list' command is given

--- src/main/java/Emu.java
+++ src/main/java/Emu.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Emu {
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
+
+        String[] storage = new String[100];
+        int count = 0;
+
         Scanner scan = new Scanner(System.in);
         System.out.println(
                 line +
@@ -17,8 +21,22 @@ public class Emu {
                         " Bye. Hope to see you again soon!\n" +
                         line);
                 break;
+            }
+            else if (response.equals("list")) {
+                int listing = 1;
+                String temp = "";
+                while (listing <= count) {
+                    temp += listing + ". " + storage[listing] + "\n";
+                    listing++;
+                }
+                System.out.println(line + temp + line);
             } else {
-                System.out.println(line + response + "\n" + line);
+                System.out.println(
+                        line +
+                        "added: " + response + "\n" +
+                        line);
+                storage[count] = response;
+                count++;
             }
         }
     }
