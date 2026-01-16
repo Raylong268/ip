f182db6df6e8bc525dcb209183a150cad042ad45
Raylong268 <shanetanjun@gmail.com>
2026-01-16 21:07:53 +0800

    Using java scanner, chatbot now echoes back text and stops running when 'bye' is inputted

--- src/main/java/Emu.java
+++ src/main/java/Emu.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Emu {
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
+        Scanner scan = new Scanner(System.in);
         System.out.println(
                 line +
                 " Hello, I'm Emu!\n" +
                 " What can I do for you?\n" +
-                line +
-                " Bye. Hope to see you again soon!\n" +
                 line);
+        while (true) {
+            String response = scan.nextLine();
+            if (response.equals("bye")) {
+                System.out.println(
+                        line +
+                        " Bye. Hope to see you again soon!\n" +
+                        line);
+                break;
+            } else {
+                System.out.println(line + response + "\n" + line);
+            }
+        }
     }
 }
