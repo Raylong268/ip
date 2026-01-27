56143467d3cb076407be102ca84bc556f355af81
Raylong268 <shanetanjun@gmail.com>
2026-01-27 20:42:50 +0800

    Fix runtest.bat to use newest Emu

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -15,15 +15,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Emu < input.txt > ACTUAL.TXT
+java -classpath ..\bin emu.Emu < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
-
-IF ERRORLEVEL 1 (
-    echo ********** Test failed **********
-    exit /b 1
-) ELSE (
-    echo ********** Test passed **********
-    exit /b 0
-)
